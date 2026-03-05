b08cc158d5324515eb00d3c234ba8f1a14eedefe
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-05 20:34:08 +0500

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f0f4ff',
+          100: '#e0e9ff',
+          200: '#c7d7fe',
+          300: '#a5bbfc',
+          400: '#8196f8',
+          500: '#6270f1',
+          600: '#4f52e5',
+          700: '#4040ca',
+          800: '#3535a3',
+          900: '#303181',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
